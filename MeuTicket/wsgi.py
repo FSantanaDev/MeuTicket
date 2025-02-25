517bcbb96c279693a16a3bb922cd88916ff68e0c
Francisco Santana <bragasan34@gmail.com>
2025-02-25 19:15:28 -0400

    Atualizações na aplicação Django

--- MeuTicket/wsgi.py
+++ MeuTicket/wsgi.py
@@ -17,17 +17,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeuTicket.settings')
 
 application = get_wsgi_application()
 
-
-django.setup()
-
-User = get_user_model()
-
-username = "admin"
-email = "admin@example.com"
-password = "admin123"
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
-    print("Superusuário criado com sucesso!")
-else:
-    print("Superusuário já existe.")
