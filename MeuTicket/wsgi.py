4d5b3fc19e757789ecb2673f271f47d680994495
Francisco Santana <bragasan34@gmail.com>
2025-02-25 14:47:40 -0400

    Criando superusuário automaticamente

--- MeuTicket/wsgi.py
+++ MeuTicket/wsgi.py
@@ -8,9 +8,26 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import django
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MeuTicket.settings')
 
 application = get_wsgi_application()
+
+
+django.setup()
+
+User = get_user_model()
+
+username = "admin"
+email = "admin@example.com"
+password = "admin123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superusuário criado com sucesso!")
+else:
+    print("Superusuário já existe.")
